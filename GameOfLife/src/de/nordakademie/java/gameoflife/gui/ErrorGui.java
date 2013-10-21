c22c9c091ce9c85e4114fd2d63f238b79db6a9c9
CaraFeelester <carafeelester@gmail.com>
2013-10-21 16:41:35 +0200

    Error Gui wird in der Mitte angezeigt und der Errortext kann jetzt mit Umbrüchen versehen und angezeigt werden. Das Fenster ist nicht mehr in seiner Größe veränderlich. Einige Codeteile wurden in Methoden ausgelagert.

--- GameOfLife/src/de/nordakademie/java/gameoflife/gui/ErrorGui.java
+++ GameOfLife/src/de/nordakademie/java/gameoflife/gui/ErrorGui.java
@@ -1,32 +1,50 @@
 package de.nordakademie.java.gameoflife.gui;
 
-import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class ErrorGui {
 	
 	public void initErrorGui(String errortext){
-	JFrame frame = new JFrame("Fehler");
+	JFrame frame = new JFrame("Fehlermeldung");
 	frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
-	frame.setSize(200, 200);
+	frame.setSize(500, 200);
+	frame.setLayout(new BorderLayout());
+	frame.setResizable(false);
+	
+	setWindowIntoScreenCenter(frame);
+	
+	JPanel title = createTitlePanel();
+	JPanel textPanel = createErrorTextPanel(errortext);
+	
+	frame.add(title, BorderLayout.NORTH);
+	frame.add(textPanel, BorderLayout.CENTER);
+	frame.add(new JButton("OK"), BorderLayout.SOUTH);
+
 	frame.setVisible(true);
-	frame.setLayout(new GridLayout(3,1));
-	
-	JPanel title = new JPanel();
-	title.add(new JLabel("Fehler!"));
-		
-	JPanel textPanel = new JPanel();
-	textPanel.add(new JLabel(errortext));
-	
-	JPanel buttonPanel = new JPanel();
-	buttonPanel.add(new JButton("OK"));
-	
-	frame.add(title);
-	frame.add(textPanel);
-	frame.add(buttonPanel);
+	}
+
+	private JPanel createErrorTextPanel(String errortext) {
+		JPanel textPanel = new JPanel();
+		textPanel.add(new JTextArea(errortext));
+		return textPanel;
+	}
+
+	private JPanel createTitlePanel() {
+		JPanel title = new JPanel();
+		title.add(new JLabel("Achtung, es ist ein Fehler aufgetreten!"));
+		return title;
+	}
+
+	private void setWindowIntoScreenCenter(JFrame frame) {
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+		frame.setLocation( (d.width- frame.getSize().width) /2,(d.height- frame.getSize().height) /2);
 	}
 }
